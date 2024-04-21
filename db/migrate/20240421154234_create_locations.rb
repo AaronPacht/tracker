3d9420478b56dcf14b21f761ff73fcb927005f78
AaronPacht <aharonpacht@gmail.com>
2024-04-21 12:22:34 -0400

    db tables

--- db/migrate/20240421154234_create_locations.rb
+++ db/migrate/20240421154234_create_locations.rb
@@ -1,7 +1,10 @@
 class CreateLocations < ActiveRecord::Migration[7.0]
   def change
     create_table :locations do |t|
-
+      t.integer "user_id"
+      t.string "coordinates"
+      t.string "animal"
+      t.integer "confirmation"
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# User.delete_all
+# Location.delete_all
+
+# user1=User.create()
+# Location.create(user_id:user1.id, coordinates: "52.520507,13.404954", animal: "bear", confirmation: 50)
+# Location.create(user_id:user1.id, coordinates: "52.519507,13.404954", animal: "bear", confirmation: 100)
+# Location.create(user_id:user1.id, coordinates: "52.520007,13.405954", animal: "deer", confirmation: 100)
+# Location.create(user_id:user1.id, coordinates: "52.520007,13.403954", animal: "deer", confirmation: 50)
+
+# Location.create(user_id:28, coordinates: "52.520507,13.405954", animal: "bear", confirmation: 50)
+# Location.create(user_id:28, coordinates: "52.519507,13.405954", animal: "deer", confirmation: 100)
+
+# Location.create(user_id:28, coordinates: "52.519507,13.403954", animal: "deer", confirmation: 50)
